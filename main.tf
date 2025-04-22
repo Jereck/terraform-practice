582b6263a09631812e222ac0916b9c5fe3ce2987
Jake Reck <jakereck@gmail.com>
2025-04-22 07:23:27 -0500

    Deploying a test server

--- main.tf
+++ main.tf
@@ -5,8 +5,31 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0e449927258d45bc4"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+            #!/bin/bash
+            yum update -y
+            yum install -y httpd
+            systemctl start httpd
+            systemctl enable httpd
+            echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+            EOF
+  
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name        = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
